f51e35d025f4c76018b01a145ed4a238cb1f3388
Bera SaiTeja <73300153+sb185460@users.noreply.github.com>
2021-11-17 19:52:16 +0530

    Refactor

--- section_1/code/0105_install_java_on_linux.sh
+++ section_1/code/0105_install_java_on_linux.sh
@@ -1,8 +1,10 @@
 # add PPA repo
 sudo add-apt-repository ppa:openjdk-r/ppa
 
+#teja
+
 # Install OpenJDK
-sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install openjdk-8-jdk
 
 # Switch to Java 8
